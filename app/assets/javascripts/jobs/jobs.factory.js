ab32afbb317b947334a295215d822141317013bc
zacscodingclub <zbaston@gmail.com>
2016-10-11 19:14:12 -0400

    build skeleton for jobs factory

---
+++ app/assets/javascripts/jobs/jobs.factory.js
@@ -0,0 +1,34 @@
+(function() {
+    'use strict';
+
+    function JobFactory($http) {
+        return {
+            getJobs: getJobs,
+            getJob: getJob,
+            createJob: createJob
+            updateJob: updateJob,
+            deleteJob: deleteJob
+        }
+
+        function getJobs() {
+            
+        }
+        function getJob() {
+
+        }
+        function createJob() {
+
+        }
+        function updateJob() {
+
+        }
+        function deleteJob() {
+
+        }
+
+    }
+
+    angular
+        .module('app')
+        .factory('JobFactory', JobFactory);
+}());
